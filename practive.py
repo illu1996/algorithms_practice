e5325a9ca7fa2e3b57bfed473f35e4f48794f5f1
Lee Ji Hyeok <illu140411@gmail.com>
2024-06-12 18:24:22 +0900

    고득점 kit

--- practive.py
+++ practive.py
@@ -1,24 +1,9 @@
-def binary_search(arr, s, e, k):
-    if s > e:
-        return
-    mid = (s+e)// 2
+import heapq
+
+def dijkstra(G, s):
+    q =[]
+    heapq.heappush(q,(0,s))
     
-    if arr[mid] == k:
-        return mid
-    if arr[mid] > k:
-        return binary_search(arr,s,mid-1,k)
-    else:
-        return binary_search(arr,mid+1,e,k)
-    
-def bi_se(arr,s,e,k):
-    
-    while s<=e:
-        mid = (s+e) // 2
-        if arr[mid] == k:
-            return mid
-        if arr[mid] > k:
-            e = mid -1
-        else:
-            s = mid + 1
-    return None
+    while q:
         
+        heapq.heappop()
