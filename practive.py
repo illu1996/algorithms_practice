1d78658c940f7a369d1762eb6cdbbcf7e7a94354
Lee Ji Hyeok <illu140411@gmail.com>
2024-07-20 19:53:52 +0900

    240720

--- practive.py
+++ practive.py
@@ -1,38 +1,23 @@
-from collections import deque
+# 슬라이딩윈도우
 
-def solution(bridge_length, weight, truck_weights):
-    # 경과시간
-    cnt = 0
-    # 다리를 건너는 트럭
-    q = deque(truck_weights)
-    
-    #현재 다리위에 있는 트럭
-    origin = [0] * bridge_length
-    bridge = deque(origin)
-    
-    while bridge:
-        
-        # 무게를 초과하지 않는다면
-        if q and sum(bridge) + q[0] <= weight:
-            bridge.popleft()
-            bridge.append(q.popleft())
-        else:
-            if q:
-                if sum(bridge) - bridge[0] + q[0] <= weight:
-                    bridge.popleft()
-                    bridge.append(q.popleft())
-                else:
-                    bridge.popleft()
-                    bridge.append(0)
-            else:
-                bridge.popleft()
-        cnt += 1
-        print(bridge, cnt)
-    
-    return cnt
+arr =  [1,2,3,4,5,6,7]
+n = 7
+k = 3
+max_cnt = 0
+for i in range(n-k+1):
+    sum_  =sum(arr[i:i+k])
+    print(sum_, end=' ')
+print(max_cnt)
+print('-------')
+arr =  [1,2,3,4,5,6,7]
+n = 7
+k = 3
 
-print(solution(2, 10, [7,4,5,6]))
 
-#2, 10, [7,4,5,6]
-#10, 12, [4,4,4,2,2,1,1,1,1]
-#10, 12, [1,1,1,1,2,2,4,4,4]
+sum_ = sum(arr[:k])
+max_cnt = sum_
+for i in range(k,n):
+    sum_ += arr[i] - arr[i-k]
+    max_cnt = max(sum_,max_cnt)
+    print(sum_, end=' ')
+print(max_cnt)
