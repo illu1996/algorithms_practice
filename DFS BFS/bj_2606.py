1e8752d685c6fccf623709c94faefe53c1683497
Lee Ji Hyeok <illu140411@gmail.com>
2024-11-01 09:10:59 +0900

    bj_2606(dfs)

--- DFS BFS/bj_2606.py
+++ DFS BFS/bj_2606.py
@@ -2,9 +2,12 @@
 # 바이러스
 
 #입력
+# 컴퓨터 개수 
 computer = int(input())
+# 연결된 수
 node = int(input())
 
+# 0번 제외하고, 1번 부터 n 번까지 list화
 connect = [[] for _ in range(computer+1)]
 
 for _ in range(node):
@@ -12,10 +15,11 @@ for _ in range(node):
     connect[s].append(e)
     connect[e].append(s)
 
-    
+# 같이 0번을 제외하고, 1번부터 n번까지 방문 처리
 visited = [0]*(computer+1)
 
 count = 0
+
 def dfs(connect, v, visited):
     global count
     visited[v] = 1
@@ -28,3 +32,4 @@ def dfs(connect, v, visited):
 
 dfs(connect, 1,visited)
 print(count)
+
