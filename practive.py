d80b612e3a2270512bae40cff08b914821e08987
illu1996 <illu140411@gmail.com>
2025-03-11 21:36:10 +0900

    Re-Setup

--- practive.py
+++ practive.py
@@ -1,38 +1,14 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
+n = int(input())
 
-def solution(bridge_length, weight, truck_weights):
-    # 경과시간
-    cnt = 0
-    # 다리를 건너는 트럭
-    q = deque(truck_weights)
-    
-    #현재 다리위에 있는 트럭
-    origin = [0] * bridge_length
-    bridge = deque(origin)
-    
-    while bridge:
-        
-        # 무게를 초과하지 않는다면
-        if q and sum(bridge) + q[0] <= weight:
-            bridge.popleft()
-            bridge.append(q.popleft())
-        else:
-            if q:
-                if sum(bridge) - bridge[0] + q[0] <= weight:
-                    bridge.popleft()
-                    bridge.append(q.popleft())
-                else:
-                    bridge.popleft()
-                    bridge.append(0)
-            else:
-                bridge.popleft()
-        cnt += 1
-        print(bridge, cnt)
-    
-    return cnt
-
-print(solution(2, 10, [7,4,5,6]))
-
-#2, 10, [7,4,5,6]
-#10, 12, [4,4,4,2,2,1,1,1,1]
-#10, 12, [1,1,1,1,2,2,4,4,4]
+arr = []
+for i in range(n):
+	x, y = map(int,input().split())
+	arr.append((x,y))
+	
+arr.sort(key=lambda x:(x[0],x[1]))
+# print(arr)
+# 출력
+for j in arr:
+	print(j[0], j[1])
