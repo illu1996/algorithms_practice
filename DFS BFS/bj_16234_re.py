95e03359f2888d80b2e48e71f9c1c82ae1db46a9
Jerry <jhlee2@mutechkorea.co.kr>
2026-03-26 11:35:28 +0900

    16234

--- DFS BFS/bj_16234_re.py
+++ DFS BFS/bj_16234_re.py
@@ -3,3 +3,10 @@
 # 골드 4
 
 n, l, r = map(int, input().split())
+person = []
+for i in range(n):
+    person.append(list(map(int, input().split())))
+
+visit = list([0] * n for _ in range(n))
+union = []
+date = 0
